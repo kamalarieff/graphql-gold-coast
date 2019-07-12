84df9959ef711d586689292f8f31d49950c1507c
Kamal Arieff <kamal.arieff@gmail.com>
2019-07-13 03:38:24 +0800

    fix unable to transpile src files

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,31 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres"
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: "postgres"
+    }
+  );
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const models = {
   User: sequelize.import("./user"),
